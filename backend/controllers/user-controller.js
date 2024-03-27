856de26c504fbbb62f70e797dafad0d0596213ff
4veg0s <tkachyov03@inbox.ru>
2024-03-27 13:14:59 +0300

    feat(backend): changed news_items.description DataType from STRING(varchar(255)) to TEXT

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -11,6 +11,7 @@ class UserController {
             }
             const {email, password} = req.body;
             const userData = await userService.registration(email, password)
+            console.log(userData);
             res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
             return res.json(userData)
         } catch (e) {
--- backend/models/news_item-model.js
+++ backend/models/news_item-model.js
@@ -5,7 +5,7 @@ const NewsItem = sequelize.define('news_item', {
     id: {type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
     image: {type: DataTypes.STRING, defaultValue: 'default_news.jpg'},
     title: {type: DataTypes.STRING, defaultValue: '', allowNull: false},
-    description: {type: DataTypes.STRING, defaultValue: ''},
+    description: {type: DataTypes.TEXT, defaultValue: ''},
     date: {type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW}
 })
 
